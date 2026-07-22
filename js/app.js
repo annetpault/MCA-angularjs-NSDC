3390391cfc89c2f5d9a53ed8ec7f4e7caebb12f1
annetpault <annetmca25@gmail.com>
2026-07-22 11:39:14 +0530

    filter and ng-switch coded

--- js/app.js
+++ js/app.js
@@ -1,20 +1,34 @@
-var app =angular.module('campus360',[]);
+var app = angular.module('campus360', []);
 
-app.controller("dashboardcontoller",function($scope){
+app.controller("dashboardcontroller", function ($scope) {
 
 
-    $scope.collegename="FISAT";
-    $scope.totalStudents=1200;
-    $scope.totalFaculty=85;
+    $scope.collegename = "FISat";
+    $scope.totalStudents = 1200;
+    $scope.totalFaculty = 85;
 
     console.log("dashboard controller is loaded");
 
-    $scope.students=[
-        "Annet","Sumedha","Taniya","Agnet"
+    $scope.students = [
+        "Annet", "Sumedha", "Taniya", "Agnet"
+    ];
+    $scope.showstudents = false;
+
+    $scope.showDashboard = true;
+
+    $scope.fees = 25600;
+
+    $scope.today = new Date();
+
+    $scope.departments = [
+        "CSE","MCA","MBA","EEE","IMCA","EIE"
     ];
 
-    $scope.addstudent=function(){
-        $scope.totalstudents++;
+    $scope.selectedDepartment = "CSE";
+
+    $scope.addstudent = function () {
+
+        $scope.totalStudents++;
+
     };
 });
-
